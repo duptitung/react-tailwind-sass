2657dd9bbe16ac97fec51d3ba92b60d0b72eba8d
dhondup.lama <dhondup.lama@yipl.com.np>
2020-09-10 15:55:52 +0545

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/sass/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center p-10">
+      <h4 className="text-xl">This is a react + tailwind + sass app</h4>
     </div>
   );
 }
